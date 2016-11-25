214c4179fa7f8883d491c551ab0e5c86f9396a23
Artsiom Vitkouski <artsiom_vitkouski@epam.com>
2016-11-25 16:04:35 +0300

    sonar-project.properties added

--- src/test/java/test/by/epam/audioorder/action/InternationalizationManagerTest.java
+++ src/test/java/test/by/epam/audioorder/action/InternationalizationManagerTest.java
@@ -13,5 +13,6 @@ public class InternationalizationManagerTest {
         Assert.assertTrue("Tracks".equals(result));
         result = InternationalizationManager.getProperty("menu.tracks", new Locale("en"));
         Assert.assertTrue("Tracks".equals(result));
+        String a = "ывпрвралфыпвафл";
     }
 }
